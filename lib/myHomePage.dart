642a27442f9c0f47f221aeeb0d54547e3d83db67
MelekPahazade <pasazademelek314@gmail.com>
2022-10-21 23:45:54 +0400

    3

--- lib/myHomePage.dart
+++ lib/myHomePage.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 import 'const/myStrings.dart';
@@ -8,31 +10,68 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        children: [
-          Column(
-            children: [
-              ElevatedButton(onPressed: () {}, child: Text(MyStrings.num1)),
-              ElevatedButton(onPressed: () {}, child: Text(MyStrings.num2)),
-              ElevatedButton(onPressed: () {}, child: Text(MyStrings.num3)),
-              ElevatedButton(onPressed: () {}, child: Text(MyStrings.num4)),
-              ElevatedButton(onPressed: () {}, child: Text(MyStrings.num5)),
-              ElevatedButton(onPressed: () {}, child: Text(MyStrings.num6)),
-              ElevatedButton(onPressed: () {}, child: Text(MyStrings.num7)),
-              ElevatedButton(onPressed: () {}, child: Text(MyStrings.num8)),
-              ElevatedButton(onPressed: () {}, child: Text(MyStrings.num9)),
-              ElevatedButton(onPressed: () {}, child: Text(MyStrings.num0)),
-            ],
-          ),
-          const SizedBox(
-            width: 20,
-          ),
-          ElevatedButton(onPressed: () {}, child: Text(MyStrings.operation1)),
-          ElevatedButton(onPressed: () {}, child: Text(MyStrings.operation2)),
-          ElevatedButton(onPressed: () {}, child: Text(MyStrings.operation3)),
-          ElevatedButton(onPressed: () {}, child: Text(MyStrings.operation4)),
-        ],
-      ),
+      body: Stack(children: [
+        Column(
+          children: [
+            ElevatedButton(onPressed: () {}, child: Text(MyStrings.num1)),
+            ElevatedButton(onPressed: () {}, child: Text(MyStrings.num2)),
+            ElevatedButton(onPressed: () {}, child: Text(MyStrings.num3)),
+            ElevatedButton(onPressed: () {}, child: Text(MyStrings.num4)),
+            ElevatedButton(onPressed: () {}, child: Text(MyStrings.num5)),
+            ElevatedButton(onPressed: () {}, child: Text(MyStrings.num6)),
+            ElevatedButton(onPressed: () {}, child: Text(MyStrings.num7)),
+            ElevatedButton(onPressed: () {}, child: Text(MyStrings.num8)),
+            ElevatedButton(onPressed: () {}, child: Text(MyStrings.num9)),
+            ElevatedButton(onPressed: () {}, child: Text(MyStrings.num0)),
+            const SizedBox(
+              height: 200,
+            ),
+            Text(MyStrings.num0)
+          ],
+        ),
+        Row(
+          children: [
+            const SizedBox(
+              width: 200,
+              height: 200,
+            ),
+            ElevatedButton(onPressed: () {}, child: Text(MyStrings.operation1)),
+            ElevatedButton(onPressed: () {}, child: Text(MyStrings.operation2)),
+            ElevatedButton(onPressed: () {}, child: Text(MyStrings.operation3)),
+            ElevatedButton(onPressed: () {}, child: Text(MyStrings.operation4)),
+            const SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ]),
     );
+    // return Scaffold(
+    //   body: Row(
+    //     children: [
+    //       Column(
+    //         children: [
+    //           ElevatedButton(onPressed: () {}, child: Text(MyStrings.num1)),
+    //           ElevatedButton(onPressed: () {}, child: Text(MyStrings.num2)),
+    //           ElevatedButton(onPressed: () {}, child: Text(MyStrings.num3)),
+    //           ElevatedButton(onPressed: () {}, child: Text(MyStrings.num4)),
+    //           ElevatedButton(onPressed: () {}, child: Text(MyStrings.num5)),
+    //           ElevatedButton(onPressed: () {}, child: Text(MyStrings.num6)),
+    //           ElevatedButton(onPressed: () {}, child: Text(MyStrings.num7)),
+    //           ElevatedButton(onPressed: () {}, child: Text(MyStrings.num8)),
+    //           ElevatedButton(onPressed: () {}, child: Text(MyStrings.num9)),
+    //           ElevatedButton(onPressed: () {}, child: Text(MyStrings.num0)),
+    //         ],
+    //       ),
+    //       const SizedBox(
+    //         width: 20,
+    //       ),
+    //       ElevatedButton(onPressed: () {}, child: Text(MyStrings.operation1)),
+    //       ElevatedButton(onPressed: () {}, child: Text(MyStrings.operation2)),
+    //       ElevatedButton(onPressed: () {}, child: Text(MyStrings.operation3)),
+    //       ElevatedButton(onPressed: () {}, child: Text(MyStrings.operation4)),
+    //     ],
+    //   ),
+    // );
   }
 }
