2da7c6b892126e4519c3dd369ffa8835a2ad76ba
MelekPahazade <pasazademelek314@gmail.com>
2022-10-21 23:31:03 +0400

    2

---
+++ lib/const/myStrings.dart
@@ -0,0 +1,16 @@
+class MyStrings {
+  static String num1 = '1';
+  static String num2 = '2';
+  static String num3 = '3';
+  static String num4 = '4';
+  static String num5 = '5';
+  static String num6 = '6';
+  static String num7 = '7';
+  static String num8 = '8';
+  static String num9 = '9';
+  static String num0 = '0';
+  static String operation1 = '+';
+  static String operation2 = '-';
+  static String operation3 = '*';
+  static String operation4 = '/';
+}
